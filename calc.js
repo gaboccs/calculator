c4169cccb43a73f6653881729cb9332273c36a3d
gabriel <gaboccs@me.com>
2023-01-14 15:29:07 -0700

    Addition implementation working

--- calc.js
+++ calc.js
@@ -13,7 +13,10 @@ reader.question("What would you like to calculate?", function(input){
 	const mathSymbol = tokens[0];
 	const num1 = Number(tokens[1]);
 	const num2 = Number(tokens[2]);
-
+	  console.log(mathSymbol, num1, num2)
+	if (mathSymbol === "+") {
+		console.log(num1 + num1)}
+	
 
 	// This line closes the connection to the command line interface.
 	reader.close()
